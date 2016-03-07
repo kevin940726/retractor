11946ef0b51cea51fd230018669aaf906a600e0b
Felix Gnass <fgnass@gmail.com>
2016-03-07 23:53:32 +0100

    return locator functions

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 /* global __retractor */
-
 exports.one = function one(el) {
-  return __retractor.findOneDOMNode(el.type.name, { props: el.props });
+  return function (driver) {
+    return driver.executeScript(
+      function (name, filter) {
+        return window.__retractor.findOneDOMNode(name, filter);
+      },
+      el.type.name, { props: el.props }
+    );
+  };
 };
 
 exports.all = function one(el) {
-  return __retractor.findAllDOMNodes(el.type.name, { props: el.props });
+  return function (driver) {
+    return driver.executeScript(
+      function (name, filter) {
+        return window.__retractor.findAllDOMNodes(name, filter);
+      },
+      el.type.name, { props: el.props }
+    );
+  };
 };
