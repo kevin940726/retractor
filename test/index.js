7fda9ba9958986b3b88df2df766af3574dc7e96f
Felix Gnass <fgnass@gmail.com>
2016-03-07 23:53:45 +0100

    add basic tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,23 @@
+const expect = require('unexpected');
+const React = require('react');
+const webdriver = require('selenium-webdriver');
 
-describe('retractor', function() {
-  it('', function() {
+const { one, all } = require('..');
 
+const TodoItem = require('./fixture/TodoItem');
+
+describe('retractor', () => {
+  const driver = new webdriver.Builder().forBrowser('chrome').build();
+
+  driver.get(`file://${__dirname}/fixture/index.html`);
+
+  it('should find a single element', () => {
+    const item = driver.findElement(one(<TodoItem />));
+    return expect(item, 'when fulfilled', 'to be a', webdriver.WebElement);
+  });
+
+  it('should find all elements', () => {
+    const items = driver.findElements(all(<TodoItem />));
+    return expect(items, 'when fulfilled', 'to have length', 2);
   });
 });
